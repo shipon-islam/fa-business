c5d61e3bbf7ef2b921f57fbda36e53e68a3fca45
shipon islam <shiponkhan566@gmail.com>
2025-09-14 13:53:51 +0600

    fixed some problem

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,8 +23,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={`font-moulpali antialiased`}>
+    <html lang="en">
+      <body className={`font-moulpali antialiased`} suppressHydrationWarning>
         <ToastProvider>{children}</ToastProvider>
         <Footer />
       </body>
